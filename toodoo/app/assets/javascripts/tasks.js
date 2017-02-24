7735c1c9d53e6b90591afad7a2c9fdad484c1661
Cronwel Noel Irias <noelirias@gmail.com>
2017-02-23 22:15:02 -0500

    starting from scratch

--- toodoo/app/assets/javascripts/tasks.js
+++
@@ -1,7 +0,0 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
-// $(function() {
-$(document).on('turbolinks:load', function() {
-  console.log('document is ready!', new Date()); // this logging is optional
-  $('#task_name').focus();
-});
--- toodoo/test.rb
+++
@@ -1,9 +0,0 @@
-def mul(a,b)
-  if (a==0 || b==0)
-    return 0
-  else
-return mul(a,b-1) + a
-  end
-end
-
-puts mul(3,5)
